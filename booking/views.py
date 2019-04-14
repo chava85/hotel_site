dc593d0cd87a40edf67b04c1cd12eece8da7d583
Chava <chavaberman@gmail.com>
2019-04-14 15:14:39 +0300

    index.html

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+# from .models import Customer, Rental
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'index.html')
--- hotel/settings.py
+++ hotel/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'booking.apps.BookingConfig'
+    'booking.apps.BookingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
