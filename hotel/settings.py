acbcd652c250f7e607f1c1046405efa14aabcf62
DanAyache <ayachedan@gmail.com>
2019-04-14 13:59:31 +0300

    change app from booking to visitors

--- hotel/settings.py
+++ hotel/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'booking.apps.BookingConfig'
+    'visitors.apps.VisitorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jerusalem'
 
 USE_I18N = True
 
