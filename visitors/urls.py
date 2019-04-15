c4067710bd0cedf102393b1cef134482695d721b
Chava <chavaberman@gmail.com>
2019-04-15 12:17:36 +0300

    models

---
+++ visitors/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'visitors'
+urlpatterns = [
+	path('', views.index, name='index'),
+
+	path('bookings/', views.bookings, name='bookings]'),
+]
